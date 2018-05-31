8cc5ad7db0925d448765f062596c9b7964035dc1
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-05-31 16:41:58 -0700

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        im cool so cool
+        <img src='https://pixabay.com/en/thumb-success-successful-fan-faust-328420/'/>
       </div>
     );
   }
