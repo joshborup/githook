9e6ffedbc54dd5a3ac798310cd4f49a661e4d1ca
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-05-31 11:48:48 -0700

    test build

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const bodyParser = require('body-parser');
 const exec = require('child_process').exec
 
+app.use(express.static(`${__dirname}/../build`));
 
 app.post('/testhook', (req, res) => {
     console.log('your just pushed your file!');
-    
-
-    let child = exec('npm run build',
+    exec('npm run build',
       function(err, stdout, stderr) {
         if (err) throw err;
         else res.status(200).send('hit');
-    });
-    child;
+    });    
 })
 
 const port = 4000;
